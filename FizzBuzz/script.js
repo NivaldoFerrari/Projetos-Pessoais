21ddcdcf950c2956769c5d40172b5ede9aec618d
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-04-23 14:41:33 -0300

    Adicionando um Algoritmo FizzBuzz.

---
+++ FizzBuzz/script.js
@@ -0,0 +1,54 @@
+//Criar um carrosel ou display
+//criar um algoritimo FizzBuzz que receba parametros e tenha timer.
+// imprimir isso na parte de tras do site ao mesmo tempo que no display.
+
+
+function fizzBuzz(entrada){
+
+    if((entrada % 3 === 0) && (entrada % 5 === 0)){
+        
+        return " FizzBuzz ";
+    }
+
+    if(entrada % 3 === 0){
+
+        return " Fizz ";
+    }
+
+    if(entrada % 5 === 0){
+        
+        return " Buzz ";
+    }
+    else{
+        return ' ' + entrada +' ';
+    }
+}
+
+function fazFB() {
+    let paragrafo = document.getElementById('fundo');
+    let amostra = document.getElementById('amostra');
+    let texto = " ";
+    let contador = 0;
+  
+    const intervalId = setInterval(() => {
+      contador++;
+      paragrafo.innerHTML =  texto =( texto + fizzBuzz(contador));
+      amostra.innerHTML = fizzBuzz(contador);
+
+      if (contador === 1000) {
+        clearInterval(intervalId);
+        console.log("Contagem finalizada");
+      }
+    }, 500); // intervalo de 0.5 segundos
+  }
+
+  
+    
+
+
+    
+    
+    
+
+    
+
