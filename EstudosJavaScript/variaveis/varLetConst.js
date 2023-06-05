8d91ca0689cfeeea472e3b3bc5b5d92e25b26d1c
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-06-04 22:49:45 -0300

    tipos variaveis js

---
+++ EstudosJavaScript/variaveis/varLetConst.js
@@ -0,0 +1,20 @@
+
+
+
+if(true){
+    var name="Nivaldo";
+}
+//Com uso de var a variavel pode ser chamada fora do seu escopo
+console.log(name);
+
+
+if(true){
+    let name1="Nivaldo";
+    console.log(name1);
+}
+//Com uso de let a variavel nao pode ser chamada fora do seu escopo
+//console.log(name1);
+
+const name2="Junior"
+console.log(name2);
+// funciona como uma  variavel mas com o valor fixo
