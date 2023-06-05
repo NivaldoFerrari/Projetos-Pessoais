337d852eec9d5549852f001c2760d76529e3440e
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-06-05 12:38:47 -0300

    Operadores matematicos

---
+++ EstudosJavaScript/operadores.js
@@ -0,0 +1,56 @@
+/*
+operadores matematicos
+
++ soma 
+- subtracao
+* multiplicacao
+/ divisao
+% resto de vivisao
+++ incremento
+-- decremento
++= incremento
+-= decremento
+
+ */
+
+let nun1, nun2,res
+
+nun1=5
+nun2=10
+res=0
+
+//soma
+res=nun1+nun2
+console.log(res)
+res=0
+
+//sub
+console.log(nun2-nun1)//tambem funciona dentro do console
+
+//multiplicacao
+console.log((nun2-nun1)*4)//Segue a ordem matematica de execucao 
+
+//divisao
+console.log((nun2+nun1)/3)
+
+// % resto de vivisao
+console.log((nun2+nun1)%2)//mostra o resto da divisao de um numero no caso 15/2=7 %=1
+
+//++ incremento
+nun1++
+console.log(nun1)
+
+//-- decremento
+nun2--
+console.log(nun2)
+
+//+= incremento
+nun1+=5 //usado para contadores para qualquer valor nao apenas 1
+console.log(nun1)
+
+//-= decremento
+ nun2-=5//usado para contadores para qualquer valor nao apenas 1
+console.log(nun2)
+
+
+
