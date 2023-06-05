126eb39d32347496526cfe8fc58a8570772822ba
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-06-05 18:17:00 -0300

    Operadores Relacionais

---
+++ EstudosJavaScript/operadoresRelacionais.js
@@ -0,0 +1,26 @@
+/* 
+Operadores que retornam verdadeiro ou falso true/false
+Operadores relacionais
+
+>  maior
+>= maior ou igual
+<  menor
+<= menor ou igual
+== igual
+=== igualdade restrita
+!= diferente
+
+*/
+
+let num1=10,num2=5,num3=10,num4="10"
+
+console.log(num1 > num2)//true
+console.log(num1 < num2)//false
+console.log(num1 <= num3)//true
+console.log(num1 == num4)//true
+console.log(num1 === num4)//false
+console.log(num1 == num4)//true
+console.log(num1 != num2)//true
+
+
+
