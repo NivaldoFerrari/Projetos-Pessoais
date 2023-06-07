1f506565cad129dd2ea2738282fbd689ae7b5d4b
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-06-07 19:52:52 -0300

    pre e pos incremento

---
+++ EstudosJavaScript/pre&posInclemento.js
@@ -0,0 +1,21 @@
+let n=10
+
+//Pos incremento
+console.log(n++)
+
+console.log(n)
+
+console.log(n--)
+
+console.log(n)
+
+//Pre incremento
+console.log(++n)
+
+console.log(n)
+
+console.log(--n)
+
+console.log(n)
+
+
