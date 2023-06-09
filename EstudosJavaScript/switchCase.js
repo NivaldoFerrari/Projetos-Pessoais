fa99cb0da698ba479994db323a63366dc1c31951
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-06-09 11:16:16 -0300

    Js exercicios

---
+++ EstudosJavaScript/switchCase.js
@@ -0,0 +1,21 @@
+let colocacao=1
+//Como um if porem muito mais especifico
+switch(colocacao){
+    case 1:
+        console.log("Primeiro Lugar")
+        break
+    case 2:
+        console.log("Segundo Lugar")
+        break
+    case 3:
+        console.log("Terceiro Lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("Premio de Participacao")
+        break
+    default:
+        console.log("Nao subiu ao podio")
+        break
+
+
+}
