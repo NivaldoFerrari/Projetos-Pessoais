c25c360ace63ebf0d8ba0439385c31628c336a76
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-06-17 21:32:10 -0300

    Funções Anônimas

---
+++ EstudosJavaScript/funcoesAnimas.js
@@ -0,0 +1,16 @@
+const anonima=function(...valores){
+    let res=0
+    for(valor of valores){
+        res+=valor
+    }
+    return res
+}
+
+console.log(anonima)//mostra apenas que se trata de uma funcao
+console.log(anonima(7,3))//retona a operacao da funcao
+
+
+//Funcao Construtor
+
+const f=new Function("v1","v2","return v1+v2")// funcao construtora anonima
+console.log(f(7,5))//retona a operacao da funcao
