462dbf545d82e2a2756f374afe9a4e2e81ca8913
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-06-18 22:14:07 -0300

    Arrow Function em Javascript.

---
+++ EstudosJavaScript/arrowFunction.js
@@ -0,0 +1,10 @@
+const soma=(v1,v2)=>{
+    return v1+v2
+}
+console.log(soma(2,10))
+
+const nome=n=>{return n}// quando a funcao recebe apenas um parametro nao precisa do ()
+console.log(nome("Nivaldo"))
+
+const add=n=>n+10//pode retornar o resultado necessario mesmo n usando return e {}
+console.log(add(12))
