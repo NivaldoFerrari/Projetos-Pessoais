4cf14804f56def267ab28d79b1a61adab774bdbf
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-07-02 11:03:49 -0300

    Praticando sobre addEventListener

--- EstudosJavaScript/addEventListener2/script.js
+++ EstudosJavaScript/addEventListener2/script.js
@@ -1,30 +1,18 @@
+const caixa1=document.querySelector("#caixa1")// recebendo os elementos da caixa 1
+const caixa2=document.querySelector("#caixa2")// recebendo os elementos da caixa 2
+const btn=document.querySelector("#btn_copiar")// recebendo o botao
+const todosCursos=[...document.querySelectorAll(".curso")]// recebendo os elementos da caixa 1
 
-const cursos=[...document.querySelectorAll(".curso")]
-
-cursos.map((elemeto)=>{
-    elemeto.addEventListener("click",(evento)=>{
-        console.log(evento)//consegue receber todas as propiedades do elemento do evento 
-        const el=evento.target
-        el.classList.add("destaque")
+todosCursos.map((elemento)=>{//loop pra adicionar ...
+    elemento.addEventListener("click",(evt)=>{//... o evento(evt) click no elemento
+        const curso=evt.target
+        curso.classList.toggle("selecionado")//toggle funciona como um pn/off onde se estiver on passa pra off e vise-versa
     })
 })
 
-
-
-
-
-
-
-
-
-/* const todos=[...document.getElementsByClassName("curso")]// getElementsByClassName retorna um HTMLCollection com o ... tranfomamos em um array
-const c1=[...document.getElementsByClassName("c1")]
-const c2=[...document.getElementsByClassName("c2")]
-
-console.log(todos)
-console.log(c1)
-console.log(c2)
-
-todos.map((elementos)=>{//usando o map para adicionar mais uma classe
-    elementos.classList.add("destaque")
-}) */
+btn.addEventListener("click",()=>{//Adicionado o evento de click ao botao
+    const cursosSelecionados=[...document.querySelectorAll(".selecionado")]//tranformando em array todos os elementos selecionados
+    cursosSelecionados.map((cursosSele)=>{//percorrento todos os elementos selecionado
+        caixa2.appendChild(cursosSele)//appendChild adicciona um filho a div caixa2
+    })
+})
