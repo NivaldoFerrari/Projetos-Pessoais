ed82b08bc9ac3010942ae1c8e55bb356f44006ae
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-07-03 12:34:22 -0300

    Exercício finalizado addEventListener

--- EstudosJavaScript/addEventListener2/script.js
+++ EstudosJavaScript/addEventListener2/script.js
@@ -1,6 +1,6 @@
 const caixa1=document.querySelector("#caixa1")// recebendo os elementos da caixa 1
 const caixa2=document.querySelector("#caixa2")// recebendo os elementos da caixa 2
-const btn=document.querySelector("#btn_copiar")// recebendo o botao
+const btn=document.querySelector("#btn_tranferir")// recebendo o botao
 const todosCursos=[...document.querySelectorAll(".curso")]// recebendo os elementos da caixa 1
 
 todosCursos.map((elemento)=>{//loop pra adicionar ...
@@ -12,7 +12,14 @@ todosCursos.map((elemento)=>{//loop pra adicionar ...
 
 btn.addEventListener("click",()=>{//Adicionado o evento de click ao botao
     const cursosSelecionados=[...document.querySelectorAll(".selecionado")]//tranformando em array todos os elementos selecionados
+    const cursosNotSelecionados=[...document.querySelectorAll(".curso:not(.selecionado)")]// .curso:not(.selecionado) Retornar os elementtos dentro de .curso que n tem a classe ,selecionado
+
+    
     cursosSelecionados.map((cursosSele)=>{//percorrento todos os elementos selecionado
         caixa2.appendChild(cursosSele)//appendChild adicciona um filho a div caixa2
     })
+    cursosNotSelecionados.map((cursosSele)=>{//percorrento todos os elementos selecionado
+        caixa1.appendChild(cursosSele)//appendChild adicciona um filho a div caixa1
+    })
 })
+
