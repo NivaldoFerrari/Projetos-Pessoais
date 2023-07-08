e4955fd6de1edfda002954f23bcd0aeadeb80b54
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-07-07 23:43:25 -0300

    removeChild Removendo elementos do DOM

--- EstudosJavaScript/removeChild/script.js
+++ EstudosJavaScript/removeChild/script.js
@@ -10,7 +10,16 @@ cursos.map((elemento,indice)=>{//elemento=contedudode cada loop, indice=numero r
     novoElemento.innerHTML=elemento//adiciono conteudo do array nele
     novoElemento.setAttribute("id","c"+indice)//adiciono um ID usando o ondice do array para ser unico
     novoElemento.setAttribute("class","curso c1")//adiciono classes para estilo CSS
-
+    
+    const lixobt=document.createElement("img")
+    lixobt.setAttribute("src","./Lixeira.png")
+    lixobt.setAttribute("class","lixeira")
+    lixobt.addEventListener("click",(evento)=>{
+        caixa1.removeChild(evento.target.parentNode)
+    })
+   
+    novoElemento.appendChild(lixobt)
+   
     caixa1.appendChild(novoElemento)//adiciono o elemento a caixa 1 
     
 
