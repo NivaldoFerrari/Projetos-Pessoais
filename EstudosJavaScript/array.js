e6b6f84536cbc0bc998161b45c806c3a62ad336d
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-07-15 23:09:49 -0300

    array de funções Javascript

--- EstudosJavaScript/array.js
+++ EstudosJavaScript/array.js
@@ -8,6 +8,13 @@ const op=[
         return res
     },
     (valores)=>{
+        let res=0
+        for(v of valores){
+            res-=v
+        }
+        return res
+    },    
+    (valores)=>{
         let res=1
         for(v of valores){
             res*=v
@@ -15,11 +22,20 @@ const op=[
         return res
     },
     (valores)=>{
+        let res=1
+        for(v of valores){
+            res/=v
+        }
+        return res
+    },
+    (valores)=>{
         for(v of valores){
             console.log(v)
         }
-    },
+    }
 ]
 console.log(op[0](valores))
 console.log(op[1](valores))
-op[2](valores)
+console.log(op[2](valores))
+console.log(op[3](valores))
+op[4](valores)
