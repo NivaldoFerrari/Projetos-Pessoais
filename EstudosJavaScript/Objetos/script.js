6c1bf8e745ed448640626ec9fdf2d904ca3b6dab
NivaldoFerrari <99571167+NivaldoFerrari@users.noreply.github.com>
2023-07-20 22:04:11 -0300

    Template Strings Javascript

---
+++ EstudosJavaScript/Objetos/script.js
@@ -0,0 +1,13 @@
+class Pessoa{
+    constructor(pnome){
+        this.nome=pnome
+    }
+}
+
+let p1=new Pessoa("Nivaldo")
+let p2=new Pessoa("Gabriela")
+let p3=new Pessoa("Speed")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
---
+++ EstudosJavaScript/TemplateStrings.js
@@ -0,0 +1,5 @@
+const nome=`Nivaldo`
+const sobrenome=`Ferrari`
+const frase=`Meu nome é ${nome} e o meu sobrenome é ${sobrenome}`//Facilita a junção de mais de uma string
+
+console.log(frase)
